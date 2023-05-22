60345b5d8896e7e225eb7b4d290a77af5a6edcb9
Sebastiaan Crisan <sebastiaancrisan@gmail.com>
2023-05-22 13:42:07 +0200

    feat: working todo app no auth

--- lib/main.dart
+++ lib/main.dart
@@ -32,85 +32,90 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
-  // final FirebaseAuth _auth = FirebaseAuth.instance;
-  // final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  // TextEditingController _todoController = TextEditingController();
-  // TextEditingController _priorityController = TextEditingController();
-  // TextEditingController _tagsController = TextEditingController();
-  // TextEditingController _recurringController = TextEditingController();
-  // late Stream<QuerySnapshot> _todoStream;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  TextEditingController _todoController = TextEditingController();
+  TextEditingController _priorityController = TextEditingController();
+  TextEditingController _tagsController = TextEditingController();
+  TextEditingController _recurringController = TextEditingController();
+  late Stream<QuerySnapshot> _todoStream;
 
   @override
   void initState() {
-    // super.initState();
+    super.initState();
     // final userId = _auth.currentUser!.uid;
-    // _todoStream = _firestore
-    //     .collection('todos')
-    //     .doc(userId)
-    //     .collection('user_todos')
-    //     .snapshots();
+    _todoStream = _firestore
+        .collection('todos')
+        .doc('testId')
+
+        /// TODO: Replace with .doc(userId)
+        .collection('user_todos')
+        .snapshots();
   }
 
-  // Future<void> _addTodo() async {
-  //   final userId = _auth.currentUser!.uid;
-  //   final todo = _todoController.text.trim();
-  //   final priority = _priorityController.text.trim();
-  //   final tags = _tagsController.text.trim();
-  //   final recurring = _recurringController.text.trim();
-  //
-  //   if (todo.isNotEmpty && priority.isNotEmpty && tags.isNotEmpty) {
-  //     await _firestore
-  //         .collection('todos')
-  //         .doc(userId)
-  //         .collection('user_todos')
-  //         .add({
-  //       'todo': todo,
-  //       'priority': priority,
-  //       'tags': tags,
-  //       'recurring': recurring
-  //     });
-  //     _todoController.clear();
-  //     _priorityController.clear();
-  //     _tagsController.clear();
-  //     _recurringController.clear();
-  //   }
-  // }
-  //
-  // Future<void> _editTodo(String todoId) async {
-  //   final userId = _auth.currentUser!.uid;
-  //   final todo = _todoController.text.trim();
-  //   final priority = _priorityController.text.trim();
-  //   final tags = _tagsController.text.trim();
-  //   final recurring = _recurringController.text.trim();
-  //
-  //   if (todo.isNotEmpty && priority.isNotEmpty && tags.isNotEmpty) {
-  //     await _firestore
-  //         .collection('todos')
-  //         .doc(userId)
-  //         .collection('user_todos')
-  //         .doc(todoId)
-  //         .update({
-  //       'todo': todo,
-  //       'priority': priority,
-  //       'tags': tags,
-  //       'recurring': recurring
-  //     });
-  //     _todoController.clear();
-  //     _priorityController.clear();
-  //     _tagsController.clear();
-  //     _recurringController.clear();
-  //   }
-  // }
-  //
-  // Future<void> _deleteTodo(String todoId) async {
-  //   final userId = _auth.currentUser!.uid;
-  //   await _firestore
-  //       .collection('todos')
-  //       .doc(userId)
-  //       .collection('user_todos')
-  //       .doc(todoId)
-  //       .delete();
-  // }
+  Future<void> _addTodo() async {
+    // final userId = _auth.currentUser!.uid;
+    const userId = 'testId';
+    final todo = _todoController.text.trim();
+    final priority = _priorityController.text.trim();
+    final tags = _tagsController.text.trim();
+    final recurring = _recurringController.text.trim();
+
+    if (todo.isNotEmpty && priority.isNotEmpty && tags.isNotEmpty) {
+      await _firestore
+          .collection('todos')
+          .doc(userId)
+          .collection('user_todos')
+          .add({
+        'todo': todo,
+        'priority': priority,
+        'tags': tags,
+        'recurring': recurring
+      });
+      _todoController.clear();
+      _priorityController.clear();
+      _tagsController.clear();
+      _recurringController.clear();
+    }
+  }
+
+  Future<void> _editTodo(String todoId) async {
+    // final userId = _auth.currentUser!.uid;
+    const userId = 'testId';
+    final todo = _todoController.text.trim();
+    final priority = _priorityController.text.trim();
+    final tags = _tagsController.text.trim();
+    final recurring = _recurringController.text.trim();
+
+    if (todo.isNotEmpty && priority.isNotEmpty && tags.isNotEmpty) {
+      await _firestore
+          .collection('todos')
+          .doc(userId)
+          .collection('user_todos')
+          .doc(todoId)
+          .update({
+        'todo': todo,
+        'priority': priority,
+        'tags': tags,
+        'recurring': recurring
+      });
+      _todoController.clear();
+      _priorityController.clear();
+      _tagsController.clear();
+      _recurringController.clear();
+    }
+  }
+
+  Future<void> _deleteTodo(String todoId) async {
+    // final userId = _auth.currentUser!.uid;
+    const userId = 'testId';
+    await _firestore
+        .collection('todos')
+        .doc(userId)
+        .collection('user_todos')
+        .doc(todoId)
+        .delete();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -121,118 +126,117 @@ class _TodoScreenState extends State<TodoScreen> {
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          // children: [
-          //   TextFormField(
-          //     controller: _todoController,
-          //     decoration: InputDecoration(labelText: 'Task'),
-          //   ),
-          //   TextFormField(
-          //     controller: _priorityController,
-          //     decoration: InputDecoration(labelText: 'Priority'),
-          //   ),
-          //   TextFormField(
-          //     controller: _tagsController,
-          //     decoration: InputDecoration(labelText: 'Tags'),
-          //   ),
-          //   TextFormField(
-          //     controller: _recurringController,
-          //     decoration: InputDecoration(labelText: 'Recurring'),
-          //   ),
-          //   SizedBox(height: 16.0),
-          //   ElevatedButton(
-          //     onPressed: _addTodo,
-          //     child: Text('Add Task'),
-          //   ),
-          //   SizedBox(height: 16.0),
-          //   Expanded(
-          //     child: StreamBuilder<QuerySnapshot>(
-          //       stream: _todoStream,
-          //       builder: (context, snapshot) {
-          //         if (snapshot.hasData) {
-          //           final todos = snapshot.data!.docs;
-          //           return ListView.builder(
-          //             itemCount: todos.length,
-          //             itemBuilder: (context, index) {
-          //               final todo =
-          //                   todos[index].data() as Map<String, dynamic>;
-          //               final todoId = todos[index].id;
-          //               return ListTile(
-          //                 title: Text(todo['todo']),
-          //                 subtitle: Column(
-          //                   crossAxisAlignment: CrossAxisAlignment.start,
-          //                   children: [
-          //                     Text('Priority: ${todo['priority']}'),
-          //                     Text('Tags: ${todo['tags']}'),
-          //                     Text('Recurring: ${todo['recurring']}'),
-          //                   ],
-          //                 ),
-          //                 trailing: IconButton(
-          //                   icon: Icon(Icons.delete),
-          //                   onPressed: () => _deleteTodo(todoId),
-          //                 ),
-          //                 onTap: () {
-          //                   _todoController.text = todo['todo'];
-          //                   _priorityController.text = todo['priority'];
-          //                   _tagsController.text = todo['tags'];
-          //                   _recurringController.text = todo['recurring'];
-          //
-          //                   showDialog(
-          //                     context: context,
-          //                     builder: (BuildContext context) {
-          //                       return AlertDialog(
-          //                         title: Text('Edit Task'),
-          //                         content: SingleChildScrollView(
-          //                           child: Column(
-          //                             children: [
-          //                               TextFormField(
-          //                                 controller: _todoController,
-          //                                 decoration: InputDecoration(
-          //                                     labelText: 'Task'),
-          //                               ),
-          //                               TextFormField(
-          //                                 controller: _priorityController,
-          //                                 decoration: InputDecoration(
-          //                                     labelText: 'Priority'),
-          //                               ),
-          //                               TextFormField(
-          //                                 controller: _tagsController,
-          //                                 decoration: InputDecoration(
-          //                                     labelText: 'Tags'),
-          //                               ),
-          //                               TextFormField(
-          //                                 controller: _recurringController,
-          //                                 decoration: InputDecoration(
-          //                                     labelText: 'Recurring'),
-          //                               ),
-          //                             ],
-          //                           ),
-          //                         ),
-          //                         actions: [
-          //                           ElevatedButton(
-          //                             onPressed: () {
-          //                               _editTodo(todoId);
-          //                               Navigator.of(context).pop();
-          //                             },
-          //                             child: Text('Save Changes'),
-          //                           ),
-          //                         ],
-          //                       );
-          //                     },
-          //                   );
-          //                 },
-          //               );
-          //             },
-          //           );
-          //         } else if (snapshot.hasError) {
-          //           return Text('Error: ${snapshot.error}');
-          //         } else {
-          //           return Center(child: CircularProgressIndicator());
-          //         }
-          //       },
-          //     ),
-          //   ),
-          // ],
-          children: [],
+          children: [
+            TextFormField(
+              controller: _todoController,
+              decoration: InputDecoration(labelText: 'Task'),
+            ),
+            TextFormField(
+              controller: _priorityController,
+              decoration: InputDecoration(labelText: 'Priority'),
+            ),
+            TextFormField(
+              controller: _tagsController,
+              decoration: InputDecoration(labelText: 'Tags'),
+            ),
+            TextFormField(
+              controller: _recurringController,
+              decoration: InputDecoration(labelText: 'Recurring'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _addTodo,
+              child: Text('Add Task'),
+            ),
+            SizedBox(height: 16.0),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _todoStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final todos = snapshot.data!.docs;
+                    return ListView.builder(
+                      itemCount: todos.length,
+                      itemBuilder: (context, index) {
+                        final todo =
+                            todos[index].data() as Map<String, dynamic>;
+                        final todoId = todos[index].id;
+                        return ListTile(
+                          title: Text(todo['todo']),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Priority: ${todo['priority']}'),
+                              Text('Tags: ${todo['tags']}'),
+                              Text('Recurring: ${todo['recurring']}'),
+                            ],
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () => _deleteTodo(todoId),
+                          ),
+                          onTap: () {
+                            _todoController.text = todo['todo'];
+                            _priorityController.text = todo['priority'];
+                            _tagsController.text = todo['tags'];
+                            _recurringController.text = todo['recurring'];
+
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Edit Task'),
+                                  content: SingleChildScrollView(
+                                    child: Column(
+                                      children: [
+                                        TextFormField(
+                                          controller: _todoController,
+                                          decoration: InputDecoration(
+                                              labelText: 'Task'),
+                                        ),
+                                        TextFormField(
+                                          controller: _priorityController,
+                                          decoration: InputDecoration(
+                                              labelText: 'Priority'),
+                                        ),
+                                        TextFormField(
+                                          controller: _tagsController,
+                                          decoration: InputDecoration(
+                                              labelText: 'Tags'),
+                                        ),
+                                        TextFormField(
+                                          controller: _recurringController,
+                                          decoration: InputDecoration(
+                                              labelText: 'Recurring'),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        _editTodo(todoId);
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('Save Changes'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
