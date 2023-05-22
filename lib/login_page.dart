41d7ffe661668f5dd9dc51abd3dbdac7ef23c908
Sebastiaan Crisan <sebastiaancrisan@gmail.com>
2023-05-22 14:08:01 +0200

    feat: add register functionality

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,6 +3,67 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'todo_screen.dart';
 
+// class LoginPage extends StatefulWidget {
+//   @override
+//   _LoginPageState createState() => _LoginPageState();
+// }
+//
+// class _LoginPageState extends State<LoginPage> {
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+//
+//   Future<void> _signInWithEmailAndPassword() async {
+//     final String email = _emailController.text.trim();
+//     final String password = _passwordController.text.trim();
+//
+//     try {
+//       UserCredential userCredential = await FirebaseAuth.instance
+//           .signInWithEmailAndPassword(email: email, password: password);
+//       // Navigate to TodoScreen passing the user's uid
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(
+//           builder: (context) => TodoScreen(userId: userCredential.user!.uid),
+//         ),
+//       );
+//     } catch (e) {
+//       // Handle sign-in errors
+//       print('Sign-in error: $e');
+//       // Show an error message to the user
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Login'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             TextFormField(
+//               controller: _emailController,
+//               decoration: InputDecoration(labelText: 'Email'),
+//             ),
+//             TextFormField(
+//               controller: _passwordController,
+//               decoration: InputDecoration(labelText: 'Password'),
+//               obscureText: true,
+//             ),
+//             SizedBox(height: 16.0),
+//             ElevatedButton(
+//               onPressed: _signInWithEmailAndPassword,
+//               child: Text('Sign In'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -33,6 +94,23 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  Future<void> _registerWithEmailAndPassword() async {
+    final String email = _emailController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      // Automatically sign in the user after successful registration
+      await _signInWithEmailAndPassword();
+    } catch (e) {
+      // Handle registration errors
+      print('Registration error: $e');
+      // Show an error message to the user
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +134,10 @@ class _LoginPageState extends State<LoginPage> {
             ElevatedButton(
               onPressed: _signInWithEmailAndPassword,
               child: Text('Sign In'),
+            ),
+            ElevatedButton(
+              onPressed: _registerWithEmailAndPassword,
+              child: Text('Register'),
             ),
           ],
         ),
